bd83af47de241dbfd1860e358860907216406e84
obaida kata <54075632+obaidakata@users.noreply.github.com>
2023-03-30 21:15:37 -0400

    ClosestFoodSearchAgent (q8) working, autograde 3/3

--- searchAgents.py
+++ searchAgents.py
@@ -523,7 +523,7 @@ class ClosestDotSearchAgent(SearchAgent):
         problem = AnyFoodSearchProblem(gameState)
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return search.breadthFirstSearch(problem)
 
 class AnyFoodSearchProblem(PositionSearchProblem):
     """
@@ -559,7 +559,7 @@ class AnyFoodSearchProblem(PositionSearchProblem):
         x, y = state
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return state in self.food.asList()
 
 def mazeDistance(point1, point2, gameState):
     """
